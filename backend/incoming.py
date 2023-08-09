cdb26252ebcd86f4c715b43a8c2c507b595e0600
Esa Varemo <git@esav.fi>
2023-08-09 04:32:19 +0300

    move config to a class

--- backend/incoming.py
+++ backend/incoming.py
@@ -4,12 +4,70 @@ from flask import Flask, request
 import json
 import requests
 import textwrap
+import sys
+import os
+
+
+class Config:
+    # Telegram bot token and chat id
+    bot_token: str = ""
+    chat_id: str = ""
+
+    @staticmethod
+    def find_file() -> str:
+        """
+        Try to find the path to the config file. Prefers command line
+        argument and defaults to './config.json' if argument is not given.
+        Checks that a file exists at the given path and aborts the program otherwise.
+        """
+        if len(sys.argv) == 2:
+            config_path = sys.argv[2]
+        else:
+            config_path = './config.json'
+
+        if not os.path.isfile(config_path):
+            print(textwrap.dedent(f"""\
+                Error: Could not find config file. Defaults to config.json in working directory.
+
+                Usage: {sys.argv[0]} [config_path]
+            """))
+            sys.exit(1)
+
+        return config_path
+
+    def is_valid(self) -> bool:
+        """
+        Checks that the config object contains the necessary keys.
+        """
+        valid = True
+
+        # Check all the type annotated properties of the Config class
+        for key in Config.__annotations__.keys():
+            if not self.__dict__.get(key):
+                print(f"Error: '{key}' not present in config file")
+                valid = False
+
+        return valid
+
+    @staticmethod
+    def load() -> "Config":
+        config_path = Config.find_file()
+
+        config = Config()
+        with open(config_path, 'r') as f:
+            j: dict = json.load(f)
+            for key, value in j.items():
+                setattr(config, key, value)
+
+        if not config.is_valid():
+            print("Error: Config file failed validation")
+            sys.exit(1)
+
+        return config
 
 
 app = Flask(__name__)
-
-with open('config.json', 'r') as f:
-    config = json.load(f)
+config = Config.load()
 
 
 def format_notification(data):
@@ -22,7 +80,7 @@ def format_notification(data):
 
 def send_notification(data):
     response = requests.post(
-        f"https://api.telegram.org/bot{config['bot_token']}/sendMessage",
+        f"https://api.telegram.org/bot{config.bot_token}/sendMessage",
         json={
             "chat_id": config['chat_id'],
             "text": format_notification(data),
